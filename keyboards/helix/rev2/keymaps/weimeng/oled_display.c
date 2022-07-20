b809f604c5c0d27304f5896fa30b652033aea2f8
Wei-Meng Lee <weimeng@staticfusion.org>
2022-07-21 02:48:17 +0800

    Put RGB LED status on secondary

--- keyboards/helix/rev2/keymaps/weimeng/oled_display.c
+++ keyboards/helix/rev2/keymaps/weimeng/oled_display.c
@@ -100,8 +100,6 @@ void render_status(void) {
     oled_write_P(led_state.caps_lock ? PSTR("CAPS") : PSTR("    "), false);
     oled_write_P(led_state.scroll_lock ? PSTR("SCLK") : PSTR("    "), false);
     oled_advance_page(true);
-    render_rgbled_status(true);
-    oled_write_P(PSTR("\n"), false);
 }
 
 bool oled_task_user(void) {
@@ -115,9 +113,8 @@ bool oled_task_user(void) {
     if (is_keyboard_master()) {
         render_status();
     } else {
-        render_helix_logo();
-        // render_rgbled_status(false);
-        // render_layer_status();
+        // render_helix_logo();
+        render_rgbled_status(true);
     }
     return false;
 }
