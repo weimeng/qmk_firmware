0cf27d93e58f021f885e035c6eabe39ddc456bf2
Wei-Meng Lee <weimeng@staticfusion.org>
2022-07-21 02:36:23 +0800

    Optimisations

--- keyboards/helix/rev2/keymaps/weimeng/keymap.c
+++ keyboards/helix/rev2/keymaps/weimeng/keymap.c
@@ -59,12 +59,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * | Ctrl |  GUI |  Cap |  Alt |  Esc | Bksp |Space |Space |   [  |   ]  |   /  | Left | Down |Right |
    * `-------------------------------------------------------------------------------------------------'
    */
-  [_QWERTY] = LAYOUT( \
-      KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,                      KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL, \
-      KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLASH, \
-      KC_GRV,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
-      KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    LOWER,   RAISE,   KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_UP,   RSFT_T(KC_ENT), \
-      KC_LCTL, KC_LGUI, KC_CAPS, KC_LALT, KC_ESC,  KC_BSPC, KC_SPC,  KC_SPC,  KC_LBRC, KC_RBRC, KC_SLSH, KC_LEFT, KC_DOWN, KC_RGHT \
+  [_QWERTY] = LAYOUT(
+      KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,                      KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,
+      KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLASH,
+      KC_GRV,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+      KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    LOWER,   RAISE,   KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_UP,   RSFT_T(KC_ENT),
+      KC_LCTL, KC_LGUI, KC_CAPS, KC_LALT, KC_ESC,  KC_BSPC, KC_SPC,  KC_SPC,  KC_LBRC, KC_RBRC, KC_SLSH, KC_LEFT, KC_DOWN, KC_RGHT
       ),
 
   /* Lower
@@ -80,12 +80,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |      |      |      |      |      | Del  |      |      |      |      |      | Home | PgDn | End  |
    * `-------------------------------------------------------------------------------------------------'
    */
-  [_LOWER] = LAYOUT( \
-      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______,                   _______, _______, KC_LBRC, KC_RBRC, _______, KC_BSPC, \
-      KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                     _______, _______, _______, _______, _______, _______, \
-      KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,                    _______, _______, _______, _______, _______, _______, \
-      _______, KC_MPRV, KC_MPLY, KC_MNXT, _______, _______, _______, _______, _______, _______, _______, _______, KC_PGUP, KC_ENT, \
-      _______, _______, _______, _______, _______, KC_DEL,  _______, _______, _______, _______, _______, KC_HOME, KC_PGDN, KC_END \
+  [_LOWER] = LAYOUT(
+      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______,                   _______, _______, KC_LBRC, KC_RBRC, _______, KC_BSPC,
+      KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                     _______, _______, _______, _______, _______, _______,
+      KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,                    _______, _______, _______, _______, _______, _______,
+      _______, KC_MPRV, KC_MPLY, KC_MNXT, _______, _______, _______, _______, _______, _______, _______, _______, KC_PGUP, KC_ENT,
+      _______, _______, _______, _______, _______, KC_DEL,  _______, _______, _______, _______, _______, KC_HOME, KC_PGDN, KC_END
       ),
 
   /* Raise
@@ -101,12 +101,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |      |      |      |      |      | Del  |      |      |      |      |      | Home | PgDn | End  |
    * `-------------------------------------------------------------------------------------------------'
    */
-  [_RAISE] = LAYOUT( \
-      KC_F1,   KC_F3,   KC_F4,   KC_F4,   KC_F5,   KC_F6,                     KC_6,    KC_7,    KC_LBRC, KC_RBRC, KC_0,    KC_DEL, \
-      KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,                    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______, \
-      _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                     KC_F6,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS, \
-      _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  _______, _______, KC_F12,  _______, _______, _______, KC_PGUP, KC_ENT, \
-      _______, _______, _______, _______, _______, KC_DEL,  _______, _______, _______, _______, _______, KC_HOME, KC_PGDN, KC_END \
+  [_RAISE] = LAYOUT(
+      KC_F1,   KC_F3,   KC_F4,   KC_F4,   KC_F5,   KC_F6,                     KC_6,    KC_7,    KC_LBRC, KC_RBRC, KC_0,    KC_DEL,
+      KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,                    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______,
+      _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                     KC_F6,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS,
+      _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  _______, _______, KC_F12,  _______, _______, _______, KC_PGUP, KC_ENT,
+      _______, _______, _______, _______, _______, KC_DEL,  _______, _______, _______, _______, _______, KC_HOME, KC_PGDN, KC_END
       ),
 
   /* Adjust (Lower + Raise)
@@ -122,12 +122,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |      |      |      |      |      |      |      |      |      |      | MODE | HUE- | SAT- | VAL- |
    * `-------------------------------------------------------------------------------------------------'
    */
-  [_ADJUST] =  LAYOUT( \
-      KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                     KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12, \
-      _______, RESET,   RGBRST,  _______, _______, _______,                   _______, _______, _______, _______, _______, KC_DEL, \
-      _______, _______, _______, AU_ON,   AU_OFF,  AG_NORM,                   AG_SWAP, QWERTY,  _______, _______, _______, _______, \
-      _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, RGB_TOG, RGB_HUI, RGB_SAI, RGB_VAI, \
-      _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, RGB_MOD, RGB_HUD, RGB_SAD, RGB_VAD \
+  [_ADJUST] =  LAYOUT(
+      KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                     KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
+      _______, RESET,   RGBRST,  _______, _______, _______,                   _______, _______, _______, _______, _______, KC_DEL,
+      _______, _______, _______, AU_ON,   AU_OFF,  AG_NORM,                   AG_SWAP, QWERTY,  _______, _______, _______, _______,
+      _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, RGB_TOG, RGB_HUI, RGB_SAI, RGB_VAI,
+      _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, RGB_MOD, RGB_HUD, RGB_SAD, RGB_VAD
       )
 };
 
